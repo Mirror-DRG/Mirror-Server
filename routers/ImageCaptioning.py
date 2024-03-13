3b12842d7480b5b7aebef888cf0c746c860cc1a0
roomie-00 <81614420+roomie-00@users.noreply.github.com>
2024-03-13 12:21:23 +0900

    MIRROR-24 feat: fast api 이미지캡셔닝 api 생성

---
+++ routers/ImageCaptioning.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, File, HTTPException
+from PIL import Image
+from transformers import pipeline
+import io
+
+
+router = APIRouter(
+    prefix="/captioning",
+    tags=["Image Captioning"]
+)
+
+
+@router.post("")
+async def receive_image(file: bytes = File(...)):
+    if not file:
+        raise HTTPException(status_code=400, detail="No file provided")
+
+    caption = image_captioning(file)
+
+    return caption
+
+def image_captioning(img):
+    img = Image.open(io.BytesIO(img))
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+
+    image_to_text = pipeline("image-to-text", model="./model")
+
+    return image_to_text(img)[0]["generated_text"]
+
