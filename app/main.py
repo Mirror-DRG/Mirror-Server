492dd2fcaba6fb6ab8b0f6916dd3aa354ba241e6
roomie-00 <81614420+roomie-00@users.noreply.github.com>
2024-03-14 15:00:59 +0900

    feat:도커 파일 생성

--- app/main.py
+++ app/main.py
@@ -16,5 +16,5 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
